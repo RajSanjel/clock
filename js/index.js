2c9a0955b65e5fc414bae217bc4eb8ebbea7c36b
RajSanjel <rajsanjel201@gmail.com>
2022-06-14 11:50:07 +0545

    Made some major changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const weekday = [
   "Sunday",
   "Monday",
@@ -11,41 +13,148 @@ const ringtone1 = new Audio("../ring/ringtone1.mp3");
 let ringtone = true;
 const stopBtn = document.querySelector(".stop");
 const submitBtn = document.querySelector(".submit");
+const snoozeBtn = document.querySelector(".snooze")
 
-submitBtn.addEventListener("click", () => {
-  alarmTime = document.querySelector("#setAlarm").value.split(":");
-  alarmHour = alarmTime[0];
-  alarmMin = alarmTime[1];
-  if (alarmTime[0] < 12) {
-    ampm = "AM";
+
+function app() {
+  displayDateAndTime()
+  showAlarm()
+  removeAlarm()
+  checkAlarm()
+  stopAlarm()
+
+}
+
+
+
+function showAlarm() {
+  if (localStorage.getItem("alarmHour") === "null") {
+    document.querySelector(".reset").classList.add('nodisp')
+    document.querySelector(".activeAlarm").innerHTML = '';
   } else {
-    ampm = "PM";
+    let showHour = localStorage.getItem('alarmHour') % 12;
+    if (showHour === 0) {
+      showHour = 12;
+    }
+    document.querySelector(".activeAlarm").innerHTML = `
+      <p class="alarmHour">${showHour}:</p>
+                  <p class="alarmMin">${localStorage.getItem('alarmMin')}:</p>
+                  <p class="alarmAmPm">${localStorage.getItem('alarmAmPm')}</p>
+                  
+      `;
+    document.querySelector(".reset").classList.remove('nodisp')
+    document.querySelector(".Alarm").classList.remove('nodisp')
   }
-  ringtone = true;
-  if (alarmHour === undefined || alarmMin === undefined) {
-document.querySelector('.error').classList.remove('nodisp');
-document.querySelector('.error').innerHTML= "Invalid Time";
+}
 
-} else {
-    document.querySelector('.error').classList.add('nodisp');
-    localStorage.setItem("alarmHour", alarmHour);
-    localStorage.setItem("alarmMin", alarmMin);
-    localStorage.setItem("alarmAmPm", ampm);
-    showActive = true;
-    document.querySelector("#setAlarm").value = '00:00';
+function checkAlarm() {
+  let date = new Date();
+  let hour = date.getHours() % 12;
+  let min = date.getMinutes();
+
+  let alarm = [
+    localStorage.getItem("alarmHour"),
+    localStorage.getItem("alarmMin"),
+  ];
+  if (alarm[0] !== null) {
+    let alarmHour = alarm[0] % 12;
+    if (alarmHour === 0) {
+      alarmHour = 12;
+    }
+
+    let alarmMin = alarm[1];
+    if (min < 10) {
+      alarmMin = "0" + alarm[1] + alarmMin;
+    } else {
+      alarmMin = alarm[1];
+    }
+    let alarmAmPm = localStorage.getItem("alarmAmPm");
+
+    if (
+      hour == alarmHour &&
+      min == alarm[1] &&
+      document.querySelector(".ap").innerHTML === alarmAmPm
+    ) {
+      if (ringtone !== false) {
+        ringtone1.play();
+        stopBtn.classList.remove("nodisp");
+        snoozeBtn.classList.remove("nodisp");
+      } else {
+        ringtone1.pause();
+        stopBtn.classList.add("nodisp");
+        snoozeBtn.classList.add("nodisp");
+        localStorage.setItem("alarmHour", null);
+        localStorage.setItem("alarmMin", null);
+        localStorage.setItem("alarmAmPm", null);
+        ringtone = true;
+      }
+    }
+
+  } else {
+    ringtone = false;
   }
-});
+}
 
+function stopAlarm() {
+  stopBtn.addEventListener("click", () => {
+    localStorage.setItem("alarmHour", null);
+    localStorage.setItem("alarmMin", null);
+    localStorage.setItem("alarmAmPm", null);
+    ringtone = true;
+    ringtone1.pause();
+    stopBtn.classList.add("nodisp");
+    snoozeBtn.classList.add("nodisp");
+  });
 
+}
 
+function addAlarm() {
+  submitBtn.addEventListener("click", () => {
+    let alarmTime = document.querySelector("#setAlarm").value.split(":");
+    let storageHour = alarmTime[0];
+    let storageMin = alarmTime[1];
+    let storageAmPm;
+    if (alarmTime[0] < 12) {
+      storageAmPm = "AM";
+    } else {
+      storageAmpM = "PM";
+    }
+    ringtone = true;
+    if (storageHour === undefined || storageMin === undefined) {
+      document.querySelector('.error').innerHTML = "Invalid Time";
+      document.querySelector('.error').classList.remove('nodisp');
+
+    } else {
+      document.querySelector('.error').classList.add('nodisp');
+      localStorage.setItem("alarmHour", storageHour);
+      localStorage.setItem("alarmMin", storageMin);
+      localStorage.setItem("alarmAmPm", storageAmPm);
+      document.querySelector("#setAlarm").value = '00:00';
+      document.querySelector(".Alarm").classList.remove('nodisp')
+      document.querySelector(".reset").classList.remove('nodisp')
+    }
+  });
+
+}
+
+function removeAlarm() {
+  document.querySelector(".reset").addEventListener("click", () => {
+    localStorage.setItem("alarmHour", null);
+    localStorage.setItem("alarmMin", null);
+    localStorage.setItem("alarmAmPm", null);
+    ringtone = true;
+    document.querySelector(".reset").classList.add('nodisp')
+    document.querySelector(".Alarm").classList.add('nodisp')
+  })
+
+}
 
 function displayDateAndTime() {
-
-
   let date = new Date();
   let hour = date.getHours() % 12;
   let min = date.getMinutes();
   let sec = date.getSeconds();
+
   if (hour === 0) {
     document.querySelector(".hour").innerHTML = "12:";
   } else {
@@ -63,7 +172,7 @@ function displayDateAndTime() {
     document.querySelector(".sec").innerHTML = sec + ":";
   }
 
-  if (date < 12) {
+  if (date.getHours() < 12) {
     document.querySelector(".ap").innerHTML = "AM";
   } else {
     document.querySelector(".ap").innerHTML = "PM";
@@ -81,83 +190,9 @@ function displayDateAndTime() {
   document.querySelector(".cdate").innerHTML = currentDate;
 
   document.querySelector(".day").innerHTML = weekday[day];
-
-  let alarm = [
-    localStorage.getItem("alarmHour"),
-    localStorage.getItem("alarmMin"),
-  ];
-  if (alarm[0] !== null) {
-    alarmHour = alarm[0] % 12;
-    if (alarmHour === 0 ){
-      alarmHour= 12;
-    }
-
-    alarmMin = alarm[1];
-    if (min < 10) {
-      alarmMin = "0" + alarm[1] + alarmMin;
-    } else {
-      alarmMin = alarm[1];
-    }
-    alarmAmPm = localStorage.getItem("alarmAmPm");
-
-    if (
-      hour == alarmHour &&
-      min == alarm[1] &&
-      document.querySelector(".ap").innerHTML === alarmAmPm
-    ) {
-      if (ringtone !== false) {
-        ringtone1.play();
-        stopBtn.classList.remove("nodisp");
-      } else {
-        ringtone1.pause();
-      }
-      stopBtn.addEventListener("click", () => {
-        ringtone1.pause();
-        stopBtn.classList.add("nodisp");
-        localStorage.setItem("alarmHour", null);
-        localStorage.setItem("alarmMin", null);
-        localStorage.setItem("alarmAmPm", null);
-        ringtone = true;
-      });
-    } else {
-      ringtone1.pause();
-      stopBtn.classList.add("nodisp");
-      if (localStorage.getItem("alarmHour" !== null)) {
-        localStorage.setItem("alarmHour", null);
-        localStorage.setItem("alarmMin", null);
-        localStorage.setItem("alarmAmPm", null);
-        ringtone = true;
-      }
-    }
-  }
-
-  if (localStorage.getItem("alarmHour") === "null") {
-    document.querySelector(".reset").classList.add('nodisp')
-    document.querySelector(".activeAlarm").innerHTML = '';
-  } else {
-    let showHour = localStorage.getItem('alarmHour') % 12;
-    if (showHour === 0 ){
-      showHour= 12;
-    }
-    document.querySelector(".activeAlarm").innerHTML = `
-      <p class="alarmHour">${showHour}:</p>
-                  <p class="alarmMin">${localStorage.getItem('alarmMin')}:</p>
-                  <p class="alarmAmPm">${localStorage.getItem('alarmAmPm')}</p>
-                  
-      `;
-    document.querySelector(".reset").classList.remove('nodisp')
-  }
-
-  document.querySelector(".reset").addEventListener("click", () => {
-    localStorage.setItem("alarmHour", null);
-    localStorage.setItem("alarmMin", null);
-    localStorage.setItem("alarmAmPm", null);
-    ringtone = true;
-  })
-
-
-
-
 }
 
-setInterval(displayDateAndTime, 100);
+
+
+addAlarm()
+setInterval(app, 100);
